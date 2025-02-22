46309a5091af67343b49552b427056d9450ef30c
Vitaliy-67 <125982107+Vitaliy-67@users.noreply.github.com>
2025-02-22 19:32:39 +0000

    Deploying to gh-pages from @ Vitaliy-67/goit-js-hw-09@cea80402307356b6b524436a2f0aed24d6c7b3db 🚀

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import"./assets/1-gallery-BmWqCRJH.js";import"./assets/2-form-B3XX4P6M.js";import{S as a}from"./assets/vendor-B-WyX8ve.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as a}from"./assets/vendor-B-WyX8ve.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=index.js.map
